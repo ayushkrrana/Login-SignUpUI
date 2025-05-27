8a3afcc029600b17af5b3313343627e60000e36b
ayushkrrana <21cse038@glbitm.ac.in>
2025-05-27 15:11:15 +0530

    Some new comments

--- lib/main.dart
+++ lib/main.dart
@@ -11,8 +11,9 @@ class LoginPageApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: SignInPage(),
+      debugShowCheckedModeBanner: false,// it is used to hide the debug banner which appears at the top right of the bottom
+      home: SignInPage(),//home is used to display the first thing you see on the display
+      // here we created three files one for the 1.SignInPage 2.SignUpPage 3.Forget password
     );
   }
 }
